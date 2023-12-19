4a4da9b4bbfad471b29515256dc7ebff759922a8
murat <murat.sady31@gmail.com>
2023-12-19 22:41:54 +0300

    Answer fields are added

--- 2d-games/QuizMaster/Assets/Scripts/QuestionSO.cs
+++ 2d-games/QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -6,4 +6,18 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text";
+    [SerializeField] string [] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion() {
+        return question;
+    }
+
+    public string GetAnswer(int index) {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
 }
