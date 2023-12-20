4e02ad7e01369ab60e5d0a7191dc77bb81036666
murat <murat.sady31@gmail.com>
2023-12-20 21:10:27 +0300

    Default and correct answer sprite are added, also OnAnswerSelected mechanism is added

--- 2d-games/QuizMaster/Assets/Scripts/Quiz.cs
+++ 2d-games/QuizMaster/Assets/Scripts/Quiz.cs
@@ -1,14 +1,40 @@
+using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour {
 
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start() {
         questionText.text = question.GetQuestion();
+
+        for(int i = 0; i<answerButtons.Length; i++) {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index) {
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+
+        //if the answer is correct
+        if(index == correctAnswerIndex) {
+            questionText.text = "Correct";
+        } else {
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was: " + correctAnswer;
+        }
+
+        Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
     }
 }
