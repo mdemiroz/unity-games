3cdd6e86fadb5a0a3275712c499b2b753cc7d6f6
murat <murat.sady31@gmail.com>
2023-12-24 16:54:19 +0300

    Hook to cancel remaining time after answering the question is added

--- 2d-games/QuizMaster/Assets/Scripts/Timer.cs
+++ 2d-games/QuizMaster/Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Timer : MonoBehaviour {
     
@@ -8,6 +9,8 @@ public class Timer : MonoBehaviour {
     [SerializeField] float timeToShowCorrectAnswer;
 
     public bool isAnsweringQuestion = false;
+    public bool loadNextQuestion;
+    public float fillFraction;
 
     float timerValue;
 
@@ -15,21 +18,32 @@ public class Timer : MonoBehaviour {
         UpdateTime();
     }
 
+    public void CancelTimer() {
+        timerValue = 0;
+    }
+
     void UpdateTime() {
         timerValue -= Time.deltaTime;
 
         if(isAnsweringQuestion) {
-            if(timerValue <= 0) {
-                isAnsweringQuestion = false;
+            if(timerValue > 0) {
+                // calculate the time circle area
+                fillFraction = timerValue / timeToCompleteQuestion;
+            } else {
                 timerValue = timeToShowCorrectAnswer;
+                isAnsweringQuestion = false;
             }
         } else {
-            if(timerValue <= 0) {
-                isAnsweringQuestion = true;
+            if(timerValue > 0) {
+                // calculate the time circle area
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            } else {
                 timerValue = timeToCompleteQuestion;
+                isAnsweringQuestion = true;
+                loadNextQuestion = true;
             }
         }
 
-        Debug.Log(timerValue);
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
     }
 }
