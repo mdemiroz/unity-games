e179178b0647327382cb4e1b7ce3f3d998435be6
murat <murat.sady31@gmail.com>
2024-01-02 21:15:40 +0300

    Horizontal movement feature is added

--- 2d-games/TileVania/Assets/Scripts/PlayerMovement.cs
+++ 2d-games/TileVania/Assets/Scripts/PlayerMovement.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
 
+    [SerializeField] float speedCoefficient = 3f;
     Vector2 moveInput;
+    Rigidbody2D playerRigidBody;
 
     void Start() {
-        
+        playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        
+        Run();
+    }
+
+    void Run() {
+        Vector2 playerVelocity = new Vector2(moveInput.x * speedCoefficient, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = playerVelocity;
     }
 
     void OnMove(InputValue inputValue) {
-        moveInput = inputValue  .Get<Vector2>();
+        moveInput = inputValue.Get<Vector2>();
         Debug.Log(moveInput);
     }
 }
