964d71f2658b97078c0b70a9b3becf9ba4976211
murat <murat.sady31@gmail.com>
2024-01-16 20:40:36 +0300

    Enemy flip feature is added

---
+++ 2d-games/TileVania/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] float enemyMoveSpeed = 1f;
+    Rigidbody2D enemyRigidBody;
+
+    void Start() {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        enemyRigidBody.velocity = new Vector2(enemyMoveSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        enemyMoveSpeed = -enemyMoveSpeed;
+        FlipEnemy();
+    }
+
+    void FlipEnemy() {
+        transform.localScale = new Vector2(-Mathf.Sign(enemyRigidBody.velocity.x), transform.localScale.y);
+    }
+}
